33f466cc783ac53a0c2020d58cadd5fbdba37509
OtacilioN <otacilio.n97@gmail.com>
2018-11-29 12:39:12 -0300

    Adapt to show user input

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ root = tk.Tk()
 screen = tk.Canvas(root)
 screen.grid()
 
+import time
+
 offsets = (
     (0, 0, 1, 0),  # top
     (1, 0, 1, 1),  # upper right
@@ -31,26 +33,27 @@ digits = (
     (0, 1, 1, 1, 1, 0, 1),  # D
     (1, 0, 0, 1, 1, 1, 1),  # E
     (1, 0, 0, 0, 1, 1, 1),  # F
-    (1, 1, 1, 1, 0, 1, 1),  # G 
-    (0, 0, 1, 0, 1, 1, 1),  # H
-    (0, 0, 1, 0, 0, 0, 0),  # I
-    (0, 1, 1, 1, 0, 0, 0),  # J
-    (0, 0, 1, 0, 1, 1, 1),  # K
-    (0, 0, 0, 0, 1, 1, 0),  # L
+    (1, 0, 1, 1, 1, 1, 0),  # G
+    (0, 1, 1, 0, 1, 1, 1),  # H
+    (0, 1, 1, 0, 0, 0, 0),  # I
+    (0, 1, 1, 1, 1, 0, 0),  # J
+    (0, 0, 0, 0, 1, 1, 1),  # K
+    (0, 0, 0, 1, 1, 1, 0),  # L
     (1, 0, 1, 0, 1, 0, 1),  # M
     (0, 0, 1, 0, 1, 0, 1),  # N
-    (0, 0, 1, 1, 1, 0, 1),  # O
+    (1, 1, 1, 1, 1, 1, 0),  # O
     (1, 1, 0, 0, 1, 1, 1),  # P
     (1, 1, 1, 0, 0, 1, 1),  # Q
     (0, 0, 0, 0, 1, 0, 1),  # R
     (1, 0, 1, 1, 0, 1, 1),  # S
     (0, 0, 0, 1, 1, 1, 1),  # T
-    (0, 0, 1, 1, 1, 0, 0),  # U
+    (0, 1, 1, 1, 1, 1, 0),  # U
     (0, 1, 0, 0, 0, 1, 1),  # V
     (0, 1, 0, 1, 0, 1, 1),  # X
     (0, 1, 0, 0, 1, 0, 1),  # Z
-    (0, 1, 1, 0, 0, 1, 1),  # Y
-    (1, 1, 0, 1, 1, 0, 1)   # W
+    (0, 1, 1, 1, 0, 1, 1),  # Y
+    (1, 1, 0, 1, 1, 0, 1),  # W
+    (0, 0, 0, 0, 0, 0, 0)   # space
 )
 
 
@@ -67,14 +70,30 @@ class Digit:
         for iid, on in zip(self.segs, digits[num]):
             self.canvas.itemconfigure(iid, state = 'normal' if on else 'hidden')
 
+def get_char_ord(mychar):
+    if mychar.isdigit():
+        return int(mychar)
+    if mychar == ' ':
+        return 36
+    else:
+        return (ord(mychar.lower())-ord('a')+10)
 
 
+user_input = input("Type the phrase to show in the display: ")
 dig = Digit(screen)
+# for character in user_input:
+#     n = get_char_ord(character)
+#     dig.show(n)
 n = 0
 def update():
     global n
-    dig.show(n)
-    n = (n+1) % 36
-    root.after(1000, update)
+    my_dig = get_char_ord(user_input[n])
+    dig.show(my_dig)
+    n = (n+1) % len(user_input)
+    if n == 0:
+        plus_time = 1000
+    else:
+        plus_time = 0
+    root.after(1000 + plus_time, update)
 root.after(1000, update)
 root.mainloop()
